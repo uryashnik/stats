f085556316c63b840c57aa4e821446c82c8f18ef
vitaliy <Vitalij.Urjashnik@privatbank.ua>
2021-10-12 10:37:01 +0300

    finish

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
 import { CsvFileReader } from './CsvFileReader';
+import { MatchReader } from './MatchReader';
 import { MatchResult } from './MatchResult';
-const reader = new CsvFileReader('src/football.csv');
-reader.read();
 
-console.log(reader.data[0]);
+// Create an object that implements the DataReader interface
+const csvFileReader = new CsvFileReader('src/football.csv');
+
+// Create an object of MatchReader and pass DataReader implementation
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
+
+// maychReader.matches
 
 let manUnitedWins = 0;
 
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
   } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
