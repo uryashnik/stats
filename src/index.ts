5dc76d24ce7b3ce5b282678bb79ba8132ef8302c
vitaliy <Vitalij.Urjashnik@privatbank.ua>
2021-10-13 08:35:07 +0300

    finnish

--- src/index.ts
+++ src/index.ts
@@ -5,16 +5,17 @@ import { ConsoleReport } from './reportTargets/ConnsoleReport';
 import { HtmlReport } from './reportTargets/HtmlReport';
 import { Summary } from './Summary';
 // Create an object that implements the DataReader interface
-const csvFileReader = new CsvFileReader('src/football.csv');
+// const csvFileReader = new CsvFileReader('src/football.csv');
 
 // Create an object of MatchReader and pass DataReader implementation
-const matchReader = new MatchReader(csvFileReader);
+const matchReader = MatchReader.fromCsv('src/football.csv');
 matchReader.load();
 
 // const summary = new Summary(
 //   new WinsAnalyzer('Man United'),
 //   new ConsoleReport()
 // );
+
 // const summary = new Summary(new WinsAnalyzer('Man United'), new HtmlReport());
 // summary.buildAndPrintReport(matchReader.matches);
 const summary1 = Summary.winsAnalysisWithHtmlReport('Man United');
